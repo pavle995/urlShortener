718b0c2d6e76dc4824309bf71356bccab65b5ef6
pavle995 <kosuticp95@gmail.com>
2022-11-19 21:22:36 +0100

    added cors

--- main.go
+++ main.go
@@ -12,6 +12,7 @@ import (
 
 func main() {
 	r := gin.Default()
+	r.Use(CORSMiddleware())
 
 	initLog()
 
@@ -35,4 +36,20 @@ func initLog() {
 		panic("Failed to open log file" + err.Error())
 	}
 	log.SetOutput(file)
+}
+
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
 }
